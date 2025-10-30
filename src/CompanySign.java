8958593c4d737388d73bca6132e4df7e07fe7cf0
THIRUNAVUKKARASU <thirunavukkarasuthiru23@gmail.com>
2025-10-30 11:02:42 +0530

    Initial

--- src/CompanySign.java
+++ src/CompanySign.java
@@ -1,4 +1,4 @@
-public class CompanySign {
+spubliclass CompanySign;{
     public static void main(String[] args) {
         if (args.length != 2) {
             System.out.println("Error: Please provide exactly two arguments.");
